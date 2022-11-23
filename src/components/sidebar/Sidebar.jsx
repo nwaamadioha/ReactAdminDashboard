a990fb2cbec35704f27c27755fbeb907c0669931
nwaamadioha <udokwu.temple960@gmail.com>
2022-11-23 13:33:28 +0100

    First commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,84 @@
+import "./sidebar.scss"
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import StoreIcon from '@mui/icons-material/Store';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import InsertChartIcon from '@mui/icons-material/InsertChart';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined';
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
+import AccountCircleIconOutlined from '@mui/icons-material/AccountCircleOutlined';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div className="top">
+                <span className="logo">Ugodesigns</span>
+            </div>
+            <hr/>
+            <div className="center">
+                <ul>
+                    <p className="title">MAIN</p>
+                    <li>
+                        <DashboardIcon className="icon"/>
+                        <span>Dashboard</span>
+                    </li>
+                    <p className="title">LISTS</p>
+                    <li>
+                        <PersonOutlineOutlinedIcon className="icon"/>
+                        <span>Users</span>
+                    </li>
+                    <li>
+                        <StoreIcon className="icon"/>
+                        <span>Products</span>
+                    </li>
+                    <li>
+                        <CreditCardIcon className="icon"/>
+                        <span>Orders</span>
+                    </li>
+                    <li>
+                        <LocalShippingIcon className="icon"/>
+                        <span>Delivery</span>
+                    </li>
+                    <p className="title">USEFUL</p>
+                    <li>
+                        <InsertChartIcon className="icon"/>
+                        <span>Stats</span>
+                    </li>
+                    <li>
+                        <NotificationsNoneIcon className="icon"/>
+                        <span>Notification</span>
+                    </li>
+                    <p className="title">SERVICE</p>
+                    <li>
+                        <SettingsSystemDaydreamOutlinedIcon className="icon"/>
+                        <span>System Health</span>
+                    </li>
+                    <li>
+                       <PsychologyOutlinedIcon className="icon"/>
+                        <span>Logs</span>
+                    </li>
+                    <li>
+                       <SettingsApplicationsIcon className="icon"/>
+                        <span>Settings</span>
+                    </li>
+                    <p className="title">USER</p>
+                    <li>
+                        <AccountCircleIconOutlined className="icon"/>
+                        <span>Profile</span>
+                    </li>
+                    <li>
+                        <ExitToAppIcon className="icon"/>
+                        <span>Logout</span>
+                    </li>
+                </ul>
+            </div>
+            <div className="bottom">color options</div>
+        </div>
+    )
+}
+
+export default Sidebar
