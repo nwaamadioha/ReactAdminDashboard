411439c405a05dad268eaabe628789c637734fbd
nwaamadioha <udokwu.temple960@gmail.com>
2022-12-03 23:49:26 +0000

    Add Link functionality to users and products on the sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -11,67 +11,76 @@ import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
 import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
 import AccountCircleIconOutlined from '@mui/icons-material/AccountCircleOutlined';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
             <div className="top">
-                <span className="logo">Ugodesigns</span>
+                <Link to="/" style={{ textDecoration: "none" }}>
+                    <span className="logo">Ugodesigns</span>
+                </Link>
             </div>
-            <hr/>
+            <hr />
             <div className="center">
                 <ul>
                     <p className="title">MAIN</p>
                     <li>
-                        <DashboardIcon className="icon"/>
+                        <DashboardIcon className="icon" />
                         <span>Dashboard</span>
                     </li>
                     <p className="title">LISTS</p>
+                    <Link to="/users" style={{ textDecoration: "none" }}>
+                        <li>
+                            <PersonOutlineOutlinedIcon className="icon" />
+                            <span>Users</span>
+                        </li>
+                    </Link>
+                    <Link to="/products" style={{ textDecoration: "none" }}>
+                        <li>
+                            <StoreIcon className="icon" />
+                            <span>Products</span>
+                        </li>
+                    </Link>
+                    <Link to="/users" style={{ textDecoration: "none" }}>
+                        <li>
+                            <CreditCardIcon className="icon" />
+                            <span>Orders</span>
+                        </li>
+                    </Link>
                     <li>
-                        <PersonOutlineOutlinedIcon className="icon"/>
-                        <span>Users</span>
-                    </li>
-                    <li>
-                        <StoreIcon className="icon"/>
-                        <span>Products</span>
-                    </li>
-                    <li>
-                        <CreditCardIcon className="icon"/>
-                        <span>Orders</span>
-                    </li>
-                    <li>
-                        <LocalShippingIcon className="icon"/>
+                        <LocalShippingIcon className="icon" />
                         <span>Delivery</span>
                     </li>
                     <p className="title">USEFUL</p>
                     <li>
-                        <InsertChartIcon className="icon"/>
+                        <InsertChartIcon className="icon" />
                         <span>Stats</span>
                     </li>
                     <li>
-                        <NotificationsNoneIcon className="icon"/>
+                        <NotificationsNoneIcon className="icon" />
                         <span>Notification</span>
                     </li>
                     <p className="title">SERVICE</p>
                     <li>
-                        <SettingsSystemDaydreamOutlinedIcon className="icon"/>
+                        <SettingsSystemDaydreamOutlinedIcon className="icon" />
                         <span>System Health</span>
                     </li>
                     <li>
-                       <PsychologyOutlinedIcon className="icon"/>
+                        <PsychologyOutlinedIcon className="icon" />
                         <span>Logs</span>
                     </li>
                     <li>
-                       <SettingsApplicationsIcon className="icon"/>
+                        <SettingsApplicationsIcon className="icon" />
                         <span>Settings</span>
                     </li>
                     <p className="title">USER</p>
                     <li>
-                        <AccountCircleIconOutlined className="icon"/>
+                        <AccountCircleIconOutlined className="icon" />
                         <span>Profile</span>
                     </li>
                     <li>
-                        <ExitToAppIcon className="icon"/>
+                        <ExitToAppIcon className="icon" />
                         <span>Logout</span>
                     </li>
                 </ul>
